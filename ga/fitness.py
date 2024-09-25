e955f35cb10acaf6cf11eb4a9360911e787c45b0
Edmond <mredmondwang@gmail.com>
2024-09-25 14:20:47 +0900

    Experiment with both pixel-wise and global perturbation magnitudes

--- ga/fitness.py
+++ ga/fitness.py
@@ -7,12 +7,11 @@ from ga.model import predict
 
 def constrained_fitness_func(ga_instance, solution, solution_idx, pixel_std, model, input_batch, original_label, pixel_constraint_weight, max_perturbation_magnitude):
 
+    # This already has the pixel constraints applied
     perturbation = torch.tensor(solution).float().reshape(input_batch.shape[1:]) # [channel, height, width] (3*224*224) instead of (64*3*224*224)
 
     # Apply pixel constraints
-    perturbation = apply_pixel_constraints(perturbation, pixel_std, pixel_constraint_weight, max_perturbation_magnitude)
-
-    print(f"After applying pixel constraints: {torch.norm(perturbation).item()}")
+    # perturbation = apply_pixel_constraints(perturbation, pixel_std, pixel_constraint_weight, max_perturbation_magnitude)
 
     perturbed_input = input_batch + perturbation.unsqueeze(0) # Unsqueeze to add the batch dimension to apply to the entire batch
     perturbed_input = torch.clamp(perturbed_input, 0, 1) # Ensure the pixel values are between 0 and 1
@@ -45,18 +44,16 @@ def apply_pixel_constraints(perturbation, pixel_std, pixel_constraint_weight, ma
     # Make sure the perturbation is within the pixel standard deviation
     lower_bound = -pixel_constraint_weight * pixel_std
     upper_bound = pixel_constraint_weight * pixel_std
+    # print(f"Perturbation before clamping: {torch.norm(perturbation).item()}")
     perturbation = torch.clamp(perturbation, lower_bound, upper_bound)
+    print(f"Perturbation after clamping: {torch.norm(perturbation).item()}")
+
     # Also apply global constraints
     perturbation_magnitude = torch.norm(perturbation).item()
 
-    print(f"Perturbation magnitude after channel-wise clamping: {perturbation_magnitude}")
-
+    # print(f"Perturbation magnitude after channel-wise clamping: {perturbation_magnitude}")
     if perturbation_magnitude > max_perturbation_magnitude:
         perturbation = perturbation * (max_perturbation_magnitude / perturbation_magnitude)
-
     print(f"Perturbation magnitude after global clamping: {torch.norm(perturbation).item()}")
 
     return perturbation
-
-
-# def calculate_snr(original_image, perturbed_image):
